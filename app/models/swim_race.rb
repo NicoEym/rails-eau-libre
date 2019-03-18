931650e222c14a83f5213f8ea3e0f2bf196ad759
nicolas <nicolas.eymenier@gmail.com>
2019-03-18 17:37:29 +0100

    addition swim race events to a swim race ok

--- app/models/swim_race.rb
+++ app/models/swim_race.rb
@@ -2,6 +2,6 @@ class SwimRace < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   geocoded_by :city_name
   after_validation :geocode, if: :will_save_change_to_city_name?
-  has_many :swim_race_events
+  has_many :swim_race_events, dependent: :destroy
   has_many :swim_events, through: :swim_race_events
 end
