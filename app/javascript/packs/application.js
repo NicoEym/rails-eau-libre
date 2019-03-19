fc8f0943959bc40da36560c3284d803861222be0
nicolas <nicolas.eymenier@gmail.com>
2019-03-19 11:55:13 +0100

    javascript add several events

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,9 @@
-import "bootstrap";
-
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
+import { addDistance } from '../components/add_distance';
 
 initMapbox();
 initAutocomplete();
+addDistance();
--- app/models/swim_event.rb
+++ app/models/swim_event.rb
@@ -1,4 +1,4 @@
 class SwimEvent < ApplicationRecord
-  has_many :swim_race_events
+  has_many :swim_race_events, dependent: :destroy
   has_many :swim_races, through: :swim_race_events
 end
