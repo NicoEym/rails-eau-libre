c5ab9c10854d118663120cb17b03000a016649cc
nicolas <nicolas.eymenier@gmail.com>
2019-03-20 14:58:30 +0100

    ok submit events

--- app/javascript/components/add_distance.js
+++ app/javascript/components/add_distance.js
@@ -1,7 +1,6 @@
 var i = 1;
 function submitAndDuplicate() {
     var original = document.getElementById('form_new_event' + i);
-    original.submit();
     var clone = original.cloneNode(true); // "deep" clone
     clone.id = "form_new_event" + ++i; // there can only be one element with an ID
      // event handlers are not cloned
@@ -14,20 +13,25 @@ const addDistance = () => {
     addButton.addEventListener('click', (e) => {
       e.preventDefault();
       submitAndDuplicate();
+      console.log(i)
     });
   }
 }
 
-const submitLastForm  = () => {
+const submitForms  = () => {
     const submitButton = document.getElementById('submit-form');
     if (submitButton) {
     submitButton.addEventListener('click', (e) => {
-    document.getElementById("form_new_event" + i).submit();
-    window.location.replace("http://localhost:3000/swim_races/");
+      for (var n = 1; n <= i; n++) {
+        console.log("toto" + n)
+        var formEvent = document.getElementById('form_new_event' + n);
+        setTimeout(function(){ formEvent.submit(); }, n * 100);
+      }
+      setTimeout(function(){ window.location.replace("http://localhost:3000/swim_races/"); }, (i + 1) * 100);
     });
   }
 }
 
 
 
-export { addDistance, submitLastForm };
+export { addDistance, submitForms };
