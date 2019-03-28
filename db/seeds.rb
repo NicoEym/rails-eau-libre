3bb76e48f0ec4aba1d02fe25973f68fa6fe4bf73
nicolas <nicolas.eymenier@gmail.com>
2019-03-28 11:47:38 +0100

    ok first seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,22 +15,34 @@ puts 'Cleaning swim event database...'
 SwimEvent.destroy_all
 puts "Seeding swim events..."
 
-SwimEvent.create(swim_race_id: 19, distance: 10)
-SwimEvent.create(swim_race_id: 19, distance: 5)
-SwimEvent.create(swim_race_id: 19, distance: 5.5)
-SwimEvent.create(swim_race_id: 22, distance: 5.250)
-SwimEvent.create(swim_race_id: 22, distance: 1.25)
-SwimEvent.create(swim_race_id: 12, distance: 2.5)
-SwimEvent.create(swim_race_id: 12, distance: 2.6)
-SwimEvent.create(swim_race_id: 11, distance: 1)
-SwimEvent.create(swim_race_id: 12, distance: 1.1)
-SwimEvent.create(swim_race_id: 24, distance: 2)
-SwimEvent.create(swim_race_id: 25, distance: 3.5)
 
-# puts 'Cleaning swim races database...'
-#SwimRace.destroy_all
-# puts "Seeding swim races..."
-# SwimRace.create(name: "Open Swim Star Strasbourg 2019", city_name: "Strasbourg", dates: DateTime.new(2019,5,19), source_url: "https://www.yaka-inscription.com/ossstrasbourg2019/select_competition", image_url: "http://www.openswimstars.com/wp-content/uploads/2018/08/Image60-768x512.jpg")
+
+puts 'Cleaning swim races database...'
+SwimRace.destroy_all
+puts "Seeding swim races..."
+
+
+swimrace = SwimRace.new
+swimrace.name = 'Open Swim Star Strasbourg 2019'
+swimrace.city_name = "Strasbourg"
+swimrace.dates = DateTime.new(2019,5,19)
+swimrace.source_url = "https://www.yaka-inscription.com/ossstrasbourg2019/select_competition", image_url: "http://www.openswimstars.com/wp-content/uploads/2018/08/Image60-768x512.jpg"
+swimrace.longitude = Geocoder.search(swimrace.city_name).first.coordinates[1]
+swimrace.latitude = Geocoder.search(swimrace.city_name).first.coordinates[0]
+swimrace.photo = "‎⁨Macintosh HD⁩ ▸ ⁨Utilisateurs⁩ ▸ ⁨nicolaseymenier⁩ ▸ ⁨Bureau⁩"
+swimrace.user_id = 1
+swimrace.save!
+
+
+
+event = SwimEvent.new
+event.distance = 2.6
+event.price = 26
+event.comment = "RESTRICTIONS
+Réservé aux personnes né(e)s avant le 19 mai 2005
+DISPONIBILITÉ
+Places limitées à : 500"
+event.swim_race_id = swimrace.id
 # SwimRace.create(name: "Open Swim Star Eze 2019", city_name: "Eze", dates: DateTime.new(2019,6,2), source_url: "https://www.yaka-inscription.com/osseze2019/select_competition", image_url: "http://www.openswimstars.com/wp-content/uploads/2018/08/HDS_4294-copie-min-768x512.jpg")
 # SwimRace.create(name: "EDF Aqua Challenge Paris 2019", city_name: "Paris", dates: DateTime.new(2019,9,2), source_url: "https://www.ffneaulibre.fr/evenements/voir/5317/edf-aqua-challenge-paris-paris-19-buttes-chaumont", image_url: "https://www.ffneaulibre.fr/images/epreuves/eau-libre-2b20ae6a.jpg")
 
