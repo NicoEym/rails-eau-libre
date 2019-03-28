35097c462cad31e37fe9271be2f7d40d8a92978f
nicolas <nicolas.eymenier@gmail.com>
2019-03-28 01:13:56 +0100

    side navbar on mobile is now working Just need to improve the design of the side menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
+import { sideMenu } from '../components/side_menu';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { addDistance, submitForms } from '../components/add_distance';
 
@@ -8,3 +9,4 @@ initMapbox();
 initAutocomplete();
 addDistance();
 submitForms();
+sideMenu();
