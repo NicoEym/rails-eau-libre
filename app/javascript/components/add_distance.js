e8c1dd5ab360fb742945296bd7441065aa02764d
nicolas <nicolas.eymenier@gmail.com>
2019-03-29 19:45:11 +0100

    ok creation of new events works fine

--- app/javascript/components/add_distance.js
+++ app/javascript/components/add_distance.js
@@ -26,10 +26,10 @@ const submitForms  = () => {
       const formsEvent = document.querySelectorAll('#form_new_event');
         formsEvent.forEach((form) => {
             console.log("toto" + i)
-            setTimeout(function(){ Rails.fire(form, 'submit'); }, i * 1000);
+            setTimeout(function(){ Rails.fire(form, 'submit'); }, i * 100);
             i = i + 1;
           });
-      setTimeout(function(){ window.location.replace("http://localhost:3000/swim_races/"); }, i * 1000);
+      setTimeout(function(){ window.location.replace("http://localhost:3000/swim_races/"); }, i * 100);
     });
   }
 }
