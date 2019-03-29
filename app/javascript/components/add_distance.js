eebbb1abe45ba2c6f14f4296414fe7172e8eac70
nicolas <nicolas.eymenier@gmail.com>
2019-03-29 21:44:44 +0100

    ok redirect to swim_races/:id after events create

--- app/javascript/components/add_distance.js
+++ app/javascript/components/add_distance.js
@@ -21,15 +21,24 @@ const addDistance = () => {
 const submitForms  = () => {
     const submitButton = document.getElementById('submit-form');
     if (submitButton) {
+      //setting the new url from a regex, in order to be able to redirect to swim_races/:id
+      //first we get the url from the current page
+    const url = window.location.href
+    //regex to get every text after the /swim_events
+    const path = /swim_events.*/g;
+    // we create a new url excluding everything in the current url after /swim_events
+    const newurl = url.replace(path, '')
+    // event listener on the validate button
     submitButton.addEventListener('click', (e) => {
       var i = 1;
       const formsEvent = document.querySelectorAll('#form_new_event');
+        //we submit each event form one after the other with a delay of 100 milliseconds
         formsEvent.forEach((form) => {
-            console.log("toto" + i)
             setTimeout(function(){ Rails.fire(form, 'submit'); }, i * 100);
             i = i + 1;
           });
-      setTimeout(function(){ window.location.replace("http://localhost:3000/swim_races/"); }, i * 100);
+        //redirect after all event form have been submitted
+      setTimeout(function(){ window.location.replace(newurl); }, i * 100);
     });
   }
 }
