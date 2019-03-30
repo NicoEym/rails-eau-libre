c367bb011a913455c442a272fc524d8a241e82b9
nicolas <nicolas.eymenier@gmail.com>
2019-03-30 01:22:01 +0100

    sweet alert configured for deletion confirmation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,39 @@ import { initMapbox } from '../plugins/init_mapbox';
 import { sideMenu } from '../components/side_menu';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { addDistance, submitForms } from '../components/add_distance';
+import { initSweetalert } from '../plugins/init_sweetalert';
+
 
 initMapbox();
 initAutocomplete();
 addDistance();
 submitForms();
 sideMenu();
+
+
+
+
+initSweetalert('#delete-race-icon', {
+ title: "Êtes vous sûr(e)?",
+  text: "Vous êtes sur le point de supprimer définitivement cette course.",
+  icon: "warning"
+}, (value) => {
+  if (value === true) {
+    console.log("je supprime")
+  // const link = document.querySelector('#delete-link');
+  //   link.click();
+  }
+});
+
+initSweetalert('#delete-event-icon', {
+ title: "Êtes vous sûr(e)?",
+  text: "Vous êtes sur le point de supprimer définitivement cette épreuve.",
+  icon: "warning"
+}, (value) => {
+  if (value===true) {
+    console.log("je supprime")
+  // const link = document.querySelector('#delete-link');
+  //   link.click();
+  }
+});
+
