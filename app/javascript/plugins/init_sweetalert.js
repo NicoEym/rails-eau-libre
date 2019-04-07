fee4844aa7dbb72097c9771305fb71df1b02380d
nicolas <nicolas.eymenier@gmail.com>
2019-04-07 17:57:25 +0200

    deleting an event/race delete the correct event/race

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -1,14 +1,22 @@
 import swal from 'sweetalert';
 
+var deleteIndex;
+
+
 const initSweetalert = (selector, options = {}, callback = () => {}) => {
   const swalButtons = document.querySelectorAll(selector);
   if (swalButtons) { // protect other pages
+    var i = 0;
     swalButtons.forEach((swalButton) => {
+      const buttonIndex = i
       swalButton.addEventListener('click', () => {
+        deleteIndex = buttonIndex;
         swal(options).then(callback); // <-- add the `.then(callback)`
       });
+      i = i + 1;
     });
   }
 };
 
-export { initSweetalert };
+export { initSweetalert, deleteIndex};
+
