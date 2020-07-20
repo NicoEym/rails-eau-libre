eb036ca58034c3f41b277f8d595b9c00d0248dfb
nicolas <nicolas.eymenier@gmail.com>
2020-07-20 20:19:49 -0300

    installed pundit

---
+++ app/policies/swim_event_policy.rb
@@ -0,0 +1,31 @@
+class SwimEventPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      @scope.where(user: @user)
+    end
+  end
+
+  def create?
+    user_is_owner_or_admin?
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    user_is_owner_or_admin?
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+
+  def destroy?
+    user_is_owner_or_admin?
+  end
+
+  private
+
+  def user_is_owner_or_admin?
+    user.admin || record.swim_race.user == user
+  end
+end
